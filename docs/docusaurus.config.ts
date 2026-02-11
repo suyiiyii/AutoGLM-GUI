f6d3b8368d1f8a2bee931e31c6d0c579b14d2e13
suyiiyii <suyiiyii@gmail.com>
2026-02-11 23:46:07 +0800

    fix(docs): point footer tutorial link to intro page

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/',
+              to: '/docs/intro',
             },
           ],
         },
